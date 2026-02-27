543e372ed2aea323ce612dd3be09b6b863876f3e
Kirthika R S <kirthikasujatha2005@gmail.com>
2026-02-27 21:05:20 +0530

    Add files via upload

---
+++ Python Logic/Kirthika_R_S_stage3.py
@@ -0,0 +1,22 @@
+name = input("Enter student name: ")
+
+mark1 = float(input("Enter marks for subject 1 (0-100): "))
+mark2 = float(input("Enter marks for subject 2 (0-100): "))
+mark3 = float(input("Enter marks for subject 3 (0-100): "))
+total = mark1 + mark2 + mark3
+percentage = (total / 300) * 100
+if percentage >= 75:
+    grade = "A"
+elif percentage >= 60:
+    grade = "B"
+elif percentage >= 40:
+    grade = "C"
+else:
+    grade = "F"
+print("\n" + name)
+print("Total:", total, "/300")
+print("Percentage:", round(percentage, 2), "%")
+print("Grade:", grade)
+
+
+
